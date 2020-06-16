00038569fb71bf5553cee0cb320fed2d766f36aa
Raj Parekh <rajparekh07@gmail.com>
2020-06-16 18:32:29 +0530

    Fix domain redirection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import { Button, Header } from 'semantic-ui-react';
+import ReactGA from 'react-ga';
+
 
 function App() {
+
+  ReactGA.initialize('UA-169628228-01');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Header size='huge' color="blue">Raj Parekh</Header>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This portfolio is under construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <p class="App-link">Meanwhile visit,</p>  
+
+        <Button.Group>
+            <Button secondary as='a' href="https://docs.google.com/document/d/1-iLJ-8OZnjc51iMDdemKqGuc1gKxEq6OGvdC3zfcbj4/export?format=pdf">Resume</Button>
+
+            <Button.Or />
+            <Button primary as='a' href="https://devdb.io/parekhraj">Profile</Button>
+            
+            <Button.Or />
+            <Button secondary as='a' href="https://in.linkedin.com/in/rajparekh07">LinkedIn</Button>
+
+          </Button.Group>
+       
       </header>
     </div>
   );
